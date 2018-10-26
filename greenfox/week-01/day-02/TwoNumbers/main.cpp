d9aea48fe0729cbb09d036b376414b59e4bdaba4
rab.peti <rab.peter97@gmail.com>
2018-10-26 13:16:49 +0200

    Task05

---
+++ greenfox/week-01/day-02/TwoNumbers/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    // Create a program that prints a few operations on two numbers: 22 and 13
+
+    // Print the result of 13 added to 22
+
+    // Print the result of 13 substracted from 22
+
+    // Print the result of 22 multiplied by 13
+
+    // Print the result of 22 divided by 13 (as a decimal fraction)
+
+    // Print the integer part of 22 divided by 13
+
+    // Print the reminder of 22 divided by 13
+
+    int elsoSzam = 22;
+    int masodikSzam = 13;
+
+    std::cout << elsoSzam+masodikSzam << std::endl;
+    std::cout << elsoSzam-masodikSzam << std::endl;
+    std::cout << elsoSzam*masodikSzam << std::endl;
+    float elsoSzamFloat = elsoSzam;
+    std::cout << elsoSzamFloat/masodikSzam << std::endl;
+    std::cout << 22/13 << std::endl;
+    std::cout << masodikSzam%elsoSzam << std::endl;
+
+    return 0;
+}
