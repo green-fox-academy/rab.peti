dfc5d838605ad8834545d84d46b0c5e579e8dcaa
rab.peti <rab.peter97@gmail.com>
2018-10-26 13:42:05 +0200

    Task09

---
+++ greenfox/week-01/day-02/BMI/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    double massInKg = 81.2;
+    double heightInM = 1.78;
+
+    // Print the Body mass index (BMI) based on these values
+
+    std::cout << "Your BMI is: " << massInKg/heightInM << std::endl;
+    return 0;
+}
