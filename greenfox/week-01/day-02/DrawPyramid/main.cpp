24367507a09e377997980b69736e13192ac87409
rab.peti <rab.peter97@gmail.com>
2018-10-26 20:19:35 +0200

    Task29

--- greenfox/week-01/day-02/DrawPyramid/main.cpp
+++ greenfox/week-01/day-02/DrawPyramid/main.cpp
@@ -13,19 +13,25 @@ int main() {
     // The pyramid should have as many lines as the number was
 
     int number = 0;
-    std::string star = "";
+    std::string star = "*";
+    std::string space = "";
     std::cout << "Give me a number:";
     std::cin >> number;
-
+    int longestLine = 1;
+    int spaceLength = 0;
+    for (int j = 1; j < number; ++j)
+    {
+        longestLine += 2;
+        space += " ";
+    }
+    spaceLength = (longestLine - 1) /2;
     for (int i = 0; i < number; ++i)
     {
-        star += "*";
-        std::cout << star << std::endl;
+        std::cout << space << star << std::endl;
+        star += "**";
+        space.erase(0,1);
+        spaceLength = spaceLength-1;
     }
 
-    std::string a = "eh";
-    std::string b = "ah";
-    a.insert(0,b);
-    std::cout << a << std::endl;
     return 0;
 }
