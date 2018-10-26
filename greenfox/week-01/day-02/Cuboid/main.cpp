7b5e25d4a8e334111a4e898cb04dbdabc4971348
rab.peti <rab.peter97@gmail.com>
2018-10-26 14:20:02 +0200

    Task12

---
+++ greenfox/week-01/day-02/Cuboid/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // Write a program that stores 3 sides of a cuboid as variables (doubles)
+    // The program should write the surface area and volume of the cuboid like:
+    //
+    // Surface Area: 600
+    // Volume: 1000
+
+    double a = 0;
+    double b = 0;
+    double c = 0;
+
+    return 0;
+}
