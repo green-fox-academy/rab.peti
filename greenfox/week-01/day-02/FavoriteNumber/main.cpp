c6f249d79733e890aea07416e668db72ee387f34
rab.peti <rab.peter97@gmail.com>
2018-10-26 13:37:52 +0200

    Task07

---
+++ greenfox/week-01/day-02/FavoriteNumber/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Store your favorite number in a variable (as an integer)
+    // And print it like this: "My favorite number is: 8"
+
+    int favNumber = 7;
+    std::cout << "My favorite number is: " << favNumber << std::endl;
+    return 0;
+}
