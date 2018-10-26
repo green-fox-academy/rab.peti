f5e30b270eccdf67d5b7d3a8155521d3c1464c00
rab.peti <rab.peter97@gmail.com>
2018-10-26 14:29:21 +0200

    Task12

--- greenfox/week-01/day-02/Cuboid/main.cpp
+++ greenfox/week-01/day-02/Cuboid/main.cpp
@@ -7,9 +7,14 @@ int main() {
     // Surface Area: 600
     // Volume: 1000
 
-    double a = 0;
-    double b = 0;
-    double c = 0;
+    double a = 5;
+    double b = 6;
+    double c = 8;
+
+    double Area = 2*(a*b+b*c+a*c);
+    double Volume = a*b*c;
+
+    std::cout << "Area of the cuboid:" << Area << "\n" << "Volume of the cuboid:" << Volume << std::endl;
 
     return 0;
 }
