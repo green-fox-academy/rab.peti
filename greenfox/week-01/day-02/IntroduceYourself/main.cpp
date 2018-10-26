de13d52c5c591e2f8bff535258d0c287571a7ff3
rab.peti <rab.peter97@gmail.com>
2018-10-26 13:06:06 +0200

    NextTaskDone

---
+++ greenfox/week-01/day-02/IntroduceYourself/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    // Write a program that prints a few details to the terminal window about you
+    // It should print each detail to a new line.
+    //  - Your name
+    //  - Your age
+    //  - Your height in meters (as a decimal fraction)
+    //
+    //  Example output:
+    //  John Doe
+    //  31
+    //  1.87
+
+    std::cout << "Peter Rab" << std::endl;
+    std::cout << 21 << std::endl;
+    std::cout << 2.05 << std::endl;
+
+    return 0;
+}
