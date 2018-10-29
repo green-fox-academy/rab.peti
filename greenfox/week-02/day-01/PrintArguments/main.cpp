8dd824110a6929d5369a7cd084f195b605180a2b
rab.peti <rab.peter97@gmail.com>
2018-10-29 12:58:47 +0100

    Task06

--- greenfox/week-02/day-01/PrintArguments/main.cpp
+++ greenfox/week-02/day-01/PrintArguments/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+
+void printParams(std::string first)
+{
+    std::cout << first << std::endl;
+}
+
+void printParams(std::string first, std::string second)
+{
+    std::cout << first << "," << second << std::endl;
+}
+
+void printParams(std::string first, std::string second, std::string third)
+{
+    std::cout << first << "," << second << "," << third <<  std::endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // - Create a function called `printParams`
+    //   which prints the input String parameters
+    // - It can have any number of parameters
+
+    // Examples
+    // printParams("first")
+    // printParams("first", "second")
+    // printParams("first", "second", "third", "fourh")
+    // ...
+
+    printParams("first");
+    printParams("first","second");
+    printParams("first","second","third");
+
     return 0;
 }
