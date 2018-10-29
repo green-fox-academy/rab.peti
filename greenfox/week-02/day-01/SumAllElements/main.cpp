5113a8bf81e12192a64d6d44585bac1797abaf4d
rab.peti <rab.peter97@gmail.com>
2018-10-29 14:29:13 +0100

    Task18

---
+++ greenfox/week-02/day-01/SumAllElements/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // - Create an array variable named `ai`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Print the sum of the elements in `ai`
+
+    int ai[] = {3,4,5,6,7};
+    int sum = 0;
+    for (int i = 0; i < sizeof(ai) / sizeof(ai[0]); ++i)
+    {
+        sum += ai[i];
+    }
+    std::cout << sum << std::endl;
+    return 0;
+}
