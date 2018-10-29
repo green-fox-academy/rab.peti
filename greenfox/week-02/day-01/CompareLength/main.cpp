77cfaf8f60e700a10fff907067e8a8de40fe6d41
rab.peti <rab.peter97@gmail.com>
2018-10-29 13:32:21 +0100

    Task08

---
+++ greenfox/week-02/day-01/CompareLength/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    // - Create an array variable named `p1`
+    //   with the following content: `[1, 2, 3]`
+    // - Create an array variable named `p2`
+    //   with the following content: `[4, 5]`
+    // - Print if `p2` has more elements than `p1`
+
+    int p1[] = {1,2,3};
+    int p2[] = {4,5};
+
+    if((sizeof(p1) / sizeof(p1[0])) > (sizeof(p2)/ sizeof(p2[0])))
+    {
+        std::cout << "p1 has more elements" << std::endl;
+        std::cout << (sizeof(p1) / sizeof(p1[0])) << std::endl;
+    }
+    else
+    {
+        std::cout << "p2 has more elements" << std::endl;
+    }
+
+    return 0;
+}
