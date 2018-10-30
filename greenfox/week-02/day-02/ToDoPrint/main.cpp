90ce0cd4d380142220a3e1f42d198474e962c40a
rab.peti <rab.peter97@gmail.com>
2018-10-30 10:16:17 +0100

    Task04

---
+++ greenfox/week-02/day-02/ToDoPrint/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    std::string todoText = " - Buy milk\n";
+    // Add "My todo:" to the beginning of the todoText
+    // Add " - Download games" to the end of the todoText
+    // Add " - Diablo" to the end of the todoText but with indention
+
+    // Expected outpt:
+
+    // My todo:
+    //  - Buy milk
+    //  - Download games
+    //      - Diablo
+
+    todoText.insert(0,"My todo:\n");
+    todoText.append(" - Download games");
+    todoText.append("\n\t - Diablo");
+
+    std::cout << todoText << std::endl;
+    return 0;
+}
