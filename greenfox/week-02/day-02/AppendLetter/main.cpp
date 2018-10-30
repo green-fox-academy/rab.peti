f8f71a44de1056c041715cb7889543bab0f4684f
rab.peti <rab.peter97@gmail.com>
2018-10-30 12:47:36 +0100

    Task08

---
+++ greenfox/week-02/day-02/AppendLetter/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+
+std::vector<std::string> appendA(const std::vector<std::string>& animals)
+{
+    std::vector<std::string> temp = animals;
+    for (int i = 0; i < animals.size(); ++i)
+    {
+            temp[i] += "a";
+            std::cout << temp[i] << "  ";
+    }
+}
+
+int main() {
+
+    std::vector<std::string> animals = {"bo", "anacond", "koal", "pand", "zebr"};
+
+    // Create a function called "appendA()" that adds "a" to every string in the "animals" list.
+    // The parameter should be a vector.
+
+    for(const auto& animal : appendA(animals))
+    {
+        std::cout << animal << " ";
+    }
+    return 0;
+}
