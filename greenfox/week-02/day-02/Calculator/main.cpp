a8bb3d643d0589beb1f3fd38bd9db64fe1284515
rab.peti <rab.peter97@gmail.com>
2018-10-31 12:56:00 +0100

    Task13

--- greenfox/week-02/day-02/Calculator/main.cpp
+++ greenfox/week-02/day-02/Calculator/main.cpp
@@ -2,34 +2,103 @@
 #include <vector>
 #include <algorithm>
 
-int calculate()
+int calculate(std::string input)
 {
-    std::string kid("* 3 4");
+    std::string kid(input);
     char space(' ');
     std::string operation("");
     int operand1(0);
+    std::string operand1Length("");
     int operand2(0);
+    std::string operand2Length("");
+    int result = 0;
+    std::size_t pos(0);
 
     std::cout << kid.size() << std::endl;
+    std::cout << kid << std::endl;
+
+    for (int j = 0; j < kid.size(); ++j)
+    {
+        if((std::find(kid.begin(),kid.end(),space)) != kid.end())
+        {
+            if(operation == "")
+            {
+                operation = kid.substr(j, j+1);
+            }
+            else if(operand1 == 0)
+            {
+                operand1 = std::stoi(kid.substr(j));
+                std::cout <<"\nI'm the size:\t"<< kid.substr(j);
+                j++;
+            }
+            else if(operand2 == 0)
+            {
+                operand2 = std::stoi(kid.substr(j));
+                std::cout <<"\nI'm the size2:\t"<< kid.substr(j);
+            }
+        }
+    }
+
+    /*
     for (int i = 0; i < kid.size(); ++i)
     {
         if(kid[i] != space && operation == "")
         {
-            operation = kid[i];
+            operation = kid.substr(i, i+1);
         }
         else if(kid[i] != space && operand1 == 0)
         {
-            std::cout << "\ni-edik elem: " << kid[i];
-            operand1 = kid[i];
+            //operand1Length = kid.substr(i);
+           // pos = operand1Length.find(" ");
+            operand1 = std::stoi(kid.substr(i));
+            std::cout <<"I'm the size:"<< kid.substr(i);
         }
         else if(kid[i] != space && operand2 == 0)
         {
-            std::cout << "\ni-edik elem2: " << kid[i];
-            operand2 = kid[i];
+            operand2 = std::stoi(kid.substr(i));
+            std::cout <<"\nI'm the size2:"<< kid.substr(i);
         }
     }
-    std::cout << kid;
-    std::cout <<"\nI'm the operation: " << operation << "\nI'm the operand1: " << operand1 << "\nI'm the operand2: " << operand2 << std::endl;
+     */
+
+    std::cout <<"\nI'm the operation:\t" << operation << "\nI'm the operand1:\t" << operand1 << "\nI'm the operand2:\t" << operand2 << std::endl;
+
+    if(operation == "+")
+    {
+        return result = operand1 + operand2;
+    }
+    else if(operation == "-")
+    {
+        return result = operand1 - operand2;
+    }
+    else if(operation == "*")
+    {
+        return result = operand1 * operand2;
+    }
+    else if(operation == "/")
+    {
+        return result = operand1 / operand2;
+    }
+    else if(operation == "%")
+    {
+        return result = operand1 % operand2;
+    }
+
+    /*
+    switch(operation)
+    {
+        case "+" : std::cout << '1';
+            break;
+        case "-" : std::cout << '2';
+            break;
+        case "*" : std::cout << '1';
+            break;
+        case "/" : std::cout << '2';
+            break;
+        case "%" : std::cout << '2';
+            break;
+    }
+    */
 }
 
 int main() {
@@ -48,12 +117,12 @@ int main() {
     // Waits for the user input
     // Print the result to the prompt
     // Exit
-/*
+
     std::string input = "";
     std::cout << "Please type in the expression:";
-    std::cin >> input;
-*/
-    calculate();
+    getline(std::cin, input);
+
+    std::cout << calculate(input);
 
 
     return 0;
