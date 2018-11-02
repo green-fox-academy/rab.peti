4e4458364f6f1281ff3ed27878091830f839840e
rab.peti <rab.peter97@gmail.com>
2018-11-02 19:09:16 +0100

    Task24

--- greenfox/week-02/day-01/Bubble/main.cpp
+++ greenfox/week-02/day-01/Bubble/main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> bubbleSort(std::vector<int> &list, int size)
-{
-    bool swapped = false;
-    for (int i = 0; i < size-1; ++i) {
-        for (int j = 0; j < size-i-1; ++j) {
-            if(list[j] > list[j+1])
-            {
-                std::swap(list[j], list[j+1]);
-                swapped = true;
+std::vector<int> bubbleSort(std::vector<int> &list, int size, bool sorting) {
+    if (sorting == false) {
+        bool swapped = false;
+        for (int i = 0; i < size - 1; ++i) {
+            for (int j = 0; j < size - i - 1; ++j) {
+                if (list[j] > list[j + 1]) {
+                    std::swap(list[j], list[j + 1]);
+                    swapped = true;
+                }
             }
+            if (swapped == false)
+                break;
         }
-        if (swapped == false)
-            break;
+    } else {
+        bool swapped = false;
+        for (int i = 0; i < size - 1; ++i) {
+            for (int j = 0; j < size - i - 1; ++j) {
+                if (list[j] < list[j + 1]) {
+                    std::swap(list[j], list[j + 1]);
+                    swapped = true;
+                }
+            }
+            if (swapped == false)
+                break;
+        }
     }
-
     return list;
 }
 
@@ -25,15 +36,22 @@ int main() {
     //  Returns a list where the elements are sorted in ascending numerical order by using bubble sort
     //  Make a second boolean parameter, if it's `true` sort that list descending
 
-    std::vector<int> numbers = {6,5,3,1,8,7,2,4};
+    std::vector<int> numbers = {6, 5, 3, 1, 8, 7, 2, 4};
     int size = numbers.size();
 
+    std::cout << "Original numbers" << std::endl;
     for (int i = 0; i < size; ++i) {
         std::cout << numbers[i];
     }
 
+    std::cout << "\nAscending numbers" << std::endl;
     for (int i = 0; i < size; ++i) {
-        std::cout << bubbleSort(numbers, size)[i];
+        std::cout << bubbleSort(numbers, size, false)[i];
+    }
+
+    std::cout << "\nDescending numbers" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cout << bubbleSort(numbers, size, true)[i];
     }
 
     return 0;
