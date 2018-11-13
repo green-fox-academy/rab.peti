71f22f57854375045065fa935b7fe0ff2c15f656
rab.peti <rab.peter97@gmail.com>
2018-11-13 13:23:15 +0100

    Task01

--- greenfox/week-04/day-02/PostIt/main.cpp
+++ greenfox/week-04/day-02/PostIt/main.cpp
@@ -4,11 +4,17 @@
 
 
 int main() {
-    PostIt postIt;
-    PostIt postIt2("black","Hello!","orange");
+    // an orange with blue text: "Idea 1"
+    // a pink with black text: "Awesome"
+    // a yellow with green text: "Superb!"
+
+    PostIt postIt("orange","Idea 1","blue");
+    PostIt postIt2("pink","Awesome","black");
+    PostIt postIt3("yellow","Superb!","green");
     std::vector<PostIt> postIts;
     postIts.push_back(postIt);
     postIts.push_back(postIt2);
+    postIts.push_back(postIt3);
 
     for (int i = 0; i < postIts.size(); ++i) {
         std::cout << "BackgroundColor is: "<< postIts[i].getBackgroundColor() << "\t Text is: " << postIts[i].getText() << "\t TextColor is: " << postIts[i].getTextColor() << std::endl;
