15df1fcf0c47b77bd18f08435a7332883eb156fc
rab.peti <rab.peter97@gmail.com>
2018-11-15 11:43:42 +0100

    Task02

--- greenfox/week-04/day-04/NumberAdder/main.cpp
+++ greenfox/week-04/day-04/NumberAdder/main.cpp
@@ -4,15 +4,13 @@ int numberAdder(int n);
 
 int main() {
     // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
-    std::cout << numberAdder(5);
+    std::cout << numberAdder(6);
     return 0;
 }
 
 int numberAdder(int n)
 {
-    int number = 0;
-    for (int i = 1; i <= n; ++i) {
-        number += i;
+    if(n >= 1) {
+        return n + numberAdder(n - 1);
     }
-    return number;
 }
