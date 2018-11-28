1194487e5c5d27ce6f15e05c16e0d1e36e6803b5
rab.peti <rab.peter97@gmail.com>
2018-11-28 10:26:54 +0100

    FleetOfThings

---
+++ greenfox/week-06/practice/OOP/Task07/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "fleet.h"
+
+int main(int argc, char* args[])
+{
+    Fleet fleet;
+    // Create a fleet of things to have this output:
+    // 1. [ ] Get milk
+    // 2. [ ] Remove the obstacles
+    // 3. [x] Stand up
+    // 4. [x] Eat lunch
+    Thing thing1("Get milk");
+    Thing thing2("Remove the obstacles");
+    Thing thing3("Stand up"); thing3.complete();
+    Thing thing4("Eat lunch"); thing4.complete();
+    fleet.add(thing1);
+    fleet.add(thing2);
+    fleet.add(thing3);
+    fleet.add(thing4);
+
+    std::cout << fleet.toString() << std::endl;
+    return 0;
+}
