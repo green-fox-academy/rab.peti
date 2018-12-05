ac7763b3cae685b440ff21d5cf157952c627e633
rab.peti <rab.peter97@gmail.com>
2018-12-05 13:34:51 +0100

    Task02

---
+++ greenfox/week-07/day-03/helloOthers/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    printf("Hello, David!\n");
+    printf("Hello, Marton!\n");
+    printf("Hello, Aron!\n");
+
+    return 0;
+}
