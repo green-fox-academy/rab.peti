4338277ee26f8eb4fd0295899d9a37df21518daa
rab.peti <rab.peter97@gmail.com>
2018-12-05 13:35:09 +0100

    Task03

---
+++ greenfox/week-07/day-03/introduceYourself/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    // Write a program that prints a few details to the terminal window about you
+    // It should print each detail to a new line.
+    //  - Your name
+    //  - Your age
+    //  - Your height in meters (as a decimal fraction)
+    //
+    //  Use variables to store the informations!
+    //
+    //  Example output:
+    //  John Doe
+    //  31
+    //  1.87
+
+    printf("Peter Rab\n21\n2.05");
+
+    return 0;
+}
