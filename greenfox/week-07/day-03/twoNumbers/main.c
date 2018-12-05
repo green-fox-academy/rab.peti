81016207ff7ad49d71e214372ea5cd75f9d6985b
rab.peti <rab.peter97@gmail.com>
2018-12-05 13:35:20 +0100

    Task04

---
+++ greenfox/week-07/day-03/twoNumbers/main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    // Create a program that prints a few operations on two numbers: 22 and 13
+
+    // Print the result of 13 added to 22
+
+    // Print the result of 13 substracted from 22
+
+    // Print the result of 22 multiplied by 13
+
+    // Print the result of 22 divided by 13 (as a decimal fraction)
+
+    // Print the reminder of 22 divided by 13
+
+    // Store the results in variables and use them when you display the result
+
+    int number13 = 13;
+    int number22 = 22;
+
+    printf("%d\n", number22+number13);
+    printf("%d\n", number22-number13);
+    printf("%d\n", number22*number13);
+    printf("%d\n", number22/number13);
+    printf("%d\n", number22%number13);
+
+    return 0;
+}
