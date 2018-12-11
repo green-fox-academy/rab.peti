4e484d0461fb7916e8b623b5691bbd7b604fc01d
rab.peti <rab.peter97@gmail.com>
2018-12-11 13:03:03 +0100

    Task03

---
+++ greenfox/week-08/day-02/deallocate_memory/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// with malloc
+// please allocate a 10 long array and fill it with even numbers then print the whole array
+// please deallocate memory without using free
+
+
+int main()
+{
+    int* pointer = NULL;
+    pointer = (int *)malloc(10 * (sizeof(int)));
+    int even = 1;
+    for (int i = 0; i < 10; ++i) {
+        pointer[i] = even;
+        even+=2;
+    }
+    for (int j = 0; j < 10; ++j) {
+        printf("%d\n",pointer[j]);
+    }
+    realloc(pointer, 0);
+
+    return 0;
+}
