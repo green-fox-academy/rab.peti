9a40f7e87e6278a1938ade3df07285cd78d86e61
rab.peti <rab.peter97@gmail.com>
2018-12-11 12:40:49 +0100

    Task06

--- greenfox/week-07/day-04/coordinate/main.c
+++ greenfox/week-07/day-04/coordinate/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "coordinates.h"
 
 // Create a struct to store coordinates (for example: int x and int y)
 // Create an array which can store coordinates, do not create multidimensonal arrays here
@@ -9,9 +10,10 @@
 int main()
 {
     srand(time(NULL));
-    int array[10];
+    coordinates array[10];
     for (int i = 0; i < sizeof(array) / sizeof(array[0]); ++i) {
-        array[i] = rand() % 20;
+        array[i].x = rand() % 20;
+        array[i].y = rand() % 20;
     }
     return 0;
 }
